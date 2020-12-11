2f8b0368ba955901a7805d20a18c1df20f138833
MateusFerreiraSilva <ferreiramateus201306@gmail.com>
2020-12-11 12:57:53 -0300

    Sessões logout funcionando

--- templates/header.php
+++ templates/header.php
@@ -1,5 +1,4 @@
 <?php
-  session_start();
   $login = isset($_SESSION["codigo"]);
 ?>
 
@@ -9,10 +8,12 @@
         <img src="../../images/mad-clinic_logo.png" alt="logo da clinica">
             <?php     
             if ($login){
-                <<< HTML
-                    <button class="btn btn-outline-dark logout_bnt">
-                     Logout
-                    </button>
+                echo <<< HTML
+                    <form action="../../templates/logout.php" method="post">
+                        <button class="btn btn-outline-dark" name="logout_bnt">
+                            Logout
+                        </button>
+                    </form>
                 HTML; 
             }else{
                 echo  <<< HTML
@@ -26,10 +27,4 @@
             ?>
         </div>
     </div>
-    <script>
-        window.onload = function {
-            logout_bnt = document.querySelector(".logout_bnt");
-            logout_bnt.onclick = () => <?php session_destroy(); ?>
-        }
-    </script>
 </header>
--- templates/nav.php
+++ templates/nav.php
@@ -1,7 +1,3 @@
-<?php
-  session_start();
-?>
-
 <nav class="navbar navbar-expand-md background-nav navbar-light">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
